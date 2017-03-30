8523cf2b61e650c638a45598b7d3f98242db8f3f
Rene Hollander <rene.hollander@hotmail.de>
2017-03-30 12:52:51 +0200

    first matching algorithm

--- main.py
+++ main.py
@@ -1,11 +1,58 @@
 from input import readowcsv
 
+def countNone(dict):
+    cnt = 0
+    for key, value in dict.items():
+        if value == None:
+            cnt += 1
+    return cnt
+
+def getNone(dict):
+    for key, value in dict.items():
+        if value == None:
+            return key
+
+def isPicked(hero, picks):
+    for key, value in picks.items():
+        if value == hero:
+            return True
+    return False
+
 heroes, players = readowcsv('Overwatch Cup - Hero Preferences.csv')
 
-for hero in heroes:
-    print(hero)
+optioncount = 4
 
-print()
+composition = ["Ana", "Lucio", "Reinhardt", "Zarya", "Tracer", "Soldier: 76"]
+lineup = ["Ra4#2632", "TheKillerKuh#2662", "potatoboy#2111", "Backfish#2465", "Rene8888#21258", "Windbreaker#2639"]
+picks = {}
+
+for hero in heroes:
+    for i in range(0, len(composition)):
+        if composition[i] == hero.name:
+            composition[i] = hero
 
 for player in players:
-    print(player)
+    player.buildWeighting(1 / optioncount, heroes)
+    for i in range(0, len(lineup)):
+        if lineup[i] == player.battletag:
+            lineup[i] = player
+
+for player in lineup:
+    picks[player] = None
+
+print("heroes=" + str(heroes))
+print("players=" + str(players))
+print("composition=" + str(composition))
+print("lineup=" + str(lineup))
+print("picks=" + str(picks))
+
+while countNone(picks) >= 1:
+    p = getNone(picks)
+    possible = p.allFromPool(composition)
+    for hero, weight in possible:
+        if not isPicked(hero, picks):
+            picks[p] = hero
+            break
+    print(picks)
+
+print("picks=" + str(picks))
--- overwatch.py
+++ overwatch.py
@@ -10,8 +10,29 @@ class Player:
         self.other_characters = []
 
     def __repr__(self):
-        return self.__str__()
+        return self.battletag
 
+    def allFromPool(self, pool):
+        all = []
+        for hero, weight in self.weighting.items():
+            if weight > 0 and hero in pool:
+                all.append((hero, weight))
+        all.sort(key=lambda tup: tup[1])
+        return all
+
+    def buildWeighting(self, gradient, heroes):
+        self.weighting = {}
+        for hero in heroes:
+            self.weighting[hero] = 0
+
+        for hero in self.other_characters:
+            self.weighting[hero] = gradient
+
+        for i in range(len(self.priority_list)):
+            l = self.priority_list[i]
+            if l is not None:
+                for hero in l:
+                    self.weighting[hero] = 1 - (i * gradient)
 
 @auto_str
 class Hero:
@@ -19,4 +40,4 @@ class Hero:
         self.name = name
 
     def __repr__(self):
-        return self.__str__()
+        return self.name
