b304899bcd187060c16e54b0d106f727c78fef7f
Rene Hollander <rene.hollander@hotmail.de>
2017-03-30 13:35:46 +0200

    improved algorithm

--- main.py
+++ main.py
@@ -22,7 +22,10 @@ heroes, players = readowcsv('Overwatch Cup - Hero Preferences.csv')
 
 optioncount = 4
 
-composition = ["Ana", "Lucio", "Reinhardt", "Zarya", "Tracer", "Soldier: 76"]
+# composition = ["Ana", "Lucio", "Reinhardt", "Zarya", "Tracer", "Soldier: 76"]
+# composition = ["Tracer", "Soldier: 76", "Winston", "Zarya", "Lucio", "Zenyatta"]
+# composition = ["Soldier: 76", "Roadhog", "Reinhardt", "Zarya", "Ana", "Lucio"]
+composition = ["Pharah", "Mercy", "Reinhardt", "Zarya", "Soldier: 76", "Lucio"]
 lineup = ["Ra4#2632", "TheKillerKuh#2662", "potatoboy#2111", "Backfish#2465", "Rene8888#21258", "Windbreaker#2639"]
 picks = {}
 
@@ -46,13 +49,22 @@ print("composition=" + str(composition))
 print("lineup=" + str(lineup))
 print("picks=" + str(picks))
 
+def getOtherPick(hero, picks):
+    for key, value in picks.items():
+        if value != None and value[0] == hero:
+            return key, value[0], value[1]
+    return None
+
 while countNone(picks) >= 1:
     p = getNone(picks)
     possible = p.allFromPool(composition)
     for hero, weight in possible:
-        if not isPicked(hero, picks):
-            picks[p] = hero
-            break
+        otherPick = getOtherPick(hero, picks)
+        if otherPick == None:
+            picks[p] = (hero, weight)
+        elif otherPick[2] <= weight:
+            picks[otherPick[0]] = None
+            picks[p] = (hero, weight)
     print(picks)
 
 print("picks=" + str(picks))
